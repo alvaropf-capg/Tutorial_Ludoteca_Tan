894f8fecbedf860f02af1d4419332401c6464b2a
alvaropf-capg <alvaro.perez-fernandez.external@capgemini.com>
2026-03-04 18:02:14 +0100

    BACKEND - Clase GameController modificada
    Clase GameController conectada con GameService

--- Backend/tutorial/src/main/java/com/ccsw/tutorial/game/GameController.java
+++ Backend/tutorial/src/main/java/com/ccsw/tutorial/game/GameController.java
@@ -4,9 +4,12 @@ import com.ccsw.tutorial.game.model.Game;
 import com.ccsw.tutorial.game.model.GameDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author ccsw
@@ -17,6 +20,13 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "*")
 public class GameController {
+
+    @Autowired
+    GameService gameService;
+
+    @Autowired
+    ModelMapper mapper;
+
     /**
      * Método para recuperar una lista de {@link Game}
      *
@@ -27,7 +37,8 @@ public class GameController {
     @Operation(summary = "Find", description = "Method that return a filtered list of Games")
     @RequestMapping(path = "", method = RequestMethod.GET)
     public List<GameDto> find(@RequestParam(value = "title", required = false) String title, @RequestParam(value = "idCategory", required = false) Long idCategory) {
-        return null;
+        List<Game> games = gameService.find(title, idCategory);
+        return games.stream().map(e -> mapper.map(e, GameDto.class)).collect(Collectors.toList());
     }
 
     /**
@@ -39,6 +50,6 @@ public class GameController {
     @Operation(summary = "Save or Update", description = "Method that saves or updates a game")
     @RequestMapping(path = { "", "/{id" }, method = RequestMethod.PUT)
     public void save(@PathVariable(name = "id", required = false) Long id, @RequestBody GameDto dto) {
-
+        gameService.save(id, dto);
     }
 }
