4e5d756fab4069c4551fec9c56d94284bffd96c4
alvaropf-capg <alvaro.perez-fernandez.external@capgemini.com>
2026-03-04 09:38:49 +0100

    FRONTEND - Añadido import zone.js y editado  metodo saveCategory
    -Añadido el import de  zone.js para solucionar error a la hora de cargar la pagina despues de conectar con BBDD
    
    -Editado método saveCategory en la clase service-category.ts para que guardo nuevas catefgorias o las edite en la BBDD

--- Frontend/tutorial/src/main.ts
+++ Frontend/tutorial/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import 'zone.js' // Para evitar este error al cargar la pagina main.ts:6  RuntimeError: NG0908: In this configuration Angular requires Zone.j
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
