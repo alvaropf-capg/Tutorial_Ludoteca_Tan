08b568f87cce1dac7e99c584b4d810720bb11753
alvaropf-capg <alvaro.perez-fernandez.external@capgemini.com>
2026-03-05 13:53:27 +0100

    BACKEND - Modificacion GameRepository
    Para tener un mejor rendimiento le decimos a Spring que quremos que haga una unica consunsulta y las subconsultas mediante join correspondiente

--- Backend/tutorial/src/main/java/com/ccsw/tutorial/game/GameRepository.java
+++ Backend/tutorial/src/main/java/com/ccsw/tutorial/game/GameRepository.java
@@ -1,11 +1,23 @@
 package com.ccsw.tutorial.game;
 
 import com.ccsw.tutorial.game.model.Game;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * @author ccsw
  */
 public interface GameRepository extends CrudRepository<Game, Long>, JpaSpecificationExecutor<Game> {
+
+    /**
+     *Para evitar tantas consultas, le decimos a Spring que queremos que haga una unica con consulta y las sub.consultas mediante join correspondiente
+     *
+     */
+    @Override
+    @EntityGraph(attributePaths = { "category", "author" })
+    List<Game> findAll(Specification<Game> spec);
 }
