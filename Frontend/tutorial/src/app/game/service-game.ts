9ea334ce8183da74db8375abaccf7503fd693a16
alvaropf-capg <alvaro.perez-fernandez.external@capgemini.com>
2026-03-09 08:31:25 +0100

    FRONTEND - Añadida las clase mock-games y modificada las clase service-game

--- Frontend/tutorial/src/app/game/service-game.ts
+++ Frontend/tutorial/src/app/game/service-game.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Game } from './model/Game';
+import { GAME_DATA } from './model/mock-games';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ServiceGame {}
+export class ServiceGame {
+  constructor() {}
+
+  public getGames(title?: string, categoryId?: number): Observable<Game[]> {
+    return of (GAME_DATA);
+  }
+
+  saveGame(game: Game): Observable<void> {
+    return of(null);
+  }
+}
