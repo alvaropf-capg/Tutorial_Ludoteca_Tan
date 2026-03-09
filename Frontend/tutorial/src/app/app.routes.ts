81fb03ba5394d1ef2ce5f530c3ecfc140dc4f920
alvaropf-capg <alvaro.perez-fernandez.external@capgemini.com>
2026-03-09 08:27:00 +0100

    FRONTEND - Añadida la principal y la ruta /games al app.routes.ts
    Añadidas las rutas ' ' y 'games' al app.routes.ts

--- Frontend/tutorial/src/app/app.routes.ts
+++ Frontend/tutorial/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/games', pathMatch: 'full' },
   {path: 'categories', loadComponent: () => import('../app/category/category-list/category-list').then(m => m.CategoryListComponent)},
   {path: 'authors', loadComponent: () => import('../app/author/author-list/author-list').then(m => m.AuthorListComponent)},
+  {path: 'games', loadComponent: () => import('../app/game/game-list/game-list').then(m => m.GameListComponent)},
 ];
