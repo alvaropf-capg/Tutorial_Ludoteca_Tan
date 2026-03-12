c6ab0639fc80d70635c8f4f32c2f5868b9ad4d1b
alvaropf-capg <alvaro.perez-fernandez.external@capgemini.com>
2026-03-12 15:20:22 +0100

    FRONTEND - Implementado el html de prestamo-edit

--- Frontend/tutorial/src/app/prestamo/prestamo-edit/prestamo-edit.ts
+++ Frontend/tutorial/src/app/prestamo/prestamo-edit/prestamo-edit.ts
@@ -1,21 +1,32 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButton, MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { ServicePrestamo } from '../service-prestamo';
-import { Prestamo } from '../model/Prestamo';
+import { CommonModule } from "@angular/common";
+import { Component, Inject, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { Prestamo } from "../model/Prestamo";
+import { Cliente } from "../../cliente/model/cliente";
+import { Game } from "../../game/model/Game";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { ServicePrestamo } from "../service-prestamo";
+import { ServiceGame } from "../../game/service-game";
+import { ServiceCliente } from "../../cliente/service-cliente";
+
 
 @Component({
   selector: 'app-prestamo-edit',
   imports: [
-    FormsModule,
-    MatButton,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
+      CommonModule,
+      FormsModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatSelectModule,
+      MatButtonModule,
+      MatIconModule,
+      MatDatepickerModule,
 ],
   templateUrl: './prestamo-edit.html',
   styleUrl: './prestamo-edit.scss',
@@ -24,20 +35,69 @@ export class PrestamoEditComponent implements OnInit {
 
   prestamo: Prestamo;
 
+
+  clientes: Cliente[] = [];
+  games: Game[] = [];
+
+
+  fechaPrestamoModel: Date | null = null;
+  fechaDevolucionModel: Date | null = null;
+
+
+
   constructor(
       public dialogRef: MatDialogRef<PrestamoEditComponent>,
       @Inject(MAT_DIALOG_DATA) public data: any,
-      private servicePrestamo: ServicePrestamo
+      private servicePrestamo: ServicePrestamo,
+      private serviceCliente: ServiceCliente,
+      private serviceGame: ServiceGame
+
     ) {}
 
     ngOnInit(): void {
-      this.prestamo = this.data.prestamo ? Object.assign({}, this.data.prestamo) : new Prestamo();
+      this.prestamo = this.data?.prestamo? Object.assign({}, this.data.prestamo): new Prestamo();
+
+
+      if (this.prestamo.fechaPrestamo) {
+            this.fechaPrestamoModel = new Date(this.prestamo.fechaPrestamo);
+          }
+      if (this.prestamo.fechaDevolucion) {
+        this.fechaDevolucionModel = new Date(this.prestamo.fechaDevolucion);
+      }
+
+      // Cargar combos
+      this.serviceCliente.getClientes().subscribe((clientes) => {
+          this.clientes = clientes;
+
+          // Si venía un cliente, reasignar la referencia desde el listado
+          if (this.prestamo.cliente) {
+            const found = this.clientes.find(c => c.id === this.prestamo.cliente.id);
+            if (found) this.prestamo.cliente = found;
+          }
+      });
+
+      this.serviceGame.getGames().subscribe((games) => {
+          this.games = games;
+
+          if (this.prestamo.game) {
+            const found = this.games.find(g => g.id === this.prestamo.game.id);
+            if (found) this.prestamo.game = found;
+          }
+      });
+    }
+
+    private toIsoDate(d: Date | null): string | undefined {
+      return d ? d.toISOString().split('T')[0] : undefined; // 'YYYY-MM-DD'
     }
 
     onSave() {
+      this.prestamo.fechaPrestamo = this.toIsoDate(this.fechaPrestamoModel);
+      this.prestamo.fechaDevolucion = this.toIsoDate(this.fechaDevolucionModel);
+
       this.servicePrestamo.savePrestamo(this.prestamo).subscribe(() => {
         this.dialogRef.close();
       });
+
     }
 
     onClose() {
