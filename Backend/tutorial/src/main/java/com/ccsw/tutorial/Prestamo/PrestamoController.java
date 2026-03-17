1f077099a99ba7e20d0fd6f7bbf920aec3c50116
alvaropf-capg <alvaro.perez-fernandez.external@capgemini.com>
2026-03-17 15:46:00 +0100

    BACKEND - Comentarios y correcciones en Prestamo

--- Backend/tutorial/src/main/java/com/ccsw/tutorial/Prestamo/PrestamoController.java
+++ Backend/tutorial/src/main/java/com/ccsw/tutorial/Prestamo/PrestamoController.java
@@ -38,11 +38,17 @@ public class PrestamoController {
      */
     @Operation(summary = "Encontrar pagina", description = "Metodo que devuelve una page de Prestamos")
     @RequestMapping(path = "", method = RequestMethod.POST)
-    public Page<PrestamoDto> findPage(@RequestBody PrestamoSearchDto dto) {
+    public Page<PrestamoDto> findPage(
+            @RequestBody PrestamoSearchDto dto) { //recibe un json con pageable, gametitle(filtro), clientename(filtro), fechaPrestamoFrom, fechaPrestamoTo(rango de fechas de prestamo), fechaDevolucionFrom, fechaDevolucionTo (rango de fechas de devolucion)
 
-        Page<PrestamoDto> page = this.prestamoService.findPage(dto);
+        Page<PrestamoDto> page = this.prestamoService.findPage(dto); //llama al servicio y envia un dto
 
-        return new PageImpl<>(page.getContent().stream().map(e -> mapper.map(e, PrestamoDto.class)).collect(Collectors.toList()), page.getPageable(), page.getTotalElements());
+        return new PageImpl<>(page.getContent() //Obtiene la lista de PrestamoDto
+                .stream()   //convierte a stream
+                .map(e -> mapper.map(e, PrestamoDto.class)) //Mapea cada elemento
+                .collect(Collectors.toList()),  //Convierte stream a List
+                page.getPageable(), //Informacion de paginacion
+                page.getTotalElements());   //Numero total de elementos
     }
 
     /**
@@ -53,7 +59,7 @@ public class PrestamoController {
      */
     @Operation(summary = "Crear o actualizar", description = "Metodo que crear o actualiza un Prestamo")
     @RequestMapping(path = { "", "/{id}" }, method = RequestMethod.PUT)
-    public void save(@PathVariable(name = "id", required = false) Long id, @RequestBody PrestamoCreateDto dto) {
+    public void save(@PathVariable(name = "id", required = false) Long id, @RequestBody PrestamoCreateDto dto) { //recibe un json con gameId, clienteId, fechaPrestamo y fechaDevolucion
 
         this.prestamoService.save(id, dto);
 
